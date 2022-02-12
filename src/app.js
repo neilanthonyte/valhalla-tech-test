e252ddd58830762bb29626777ca927a9c8ffb993
Neil Anthony Te <neilanthonyte@Neils-MacBook-Pro.local>
2022-02-12 13:01:59 +0800

    setup tabs

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Pages from './pages';
 
-const Stack = createNativeStackNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Nature" component={Pages.Nature} />
-                <Stack.Screen name="Architecture" component={Pages.Architecture} />
-                <Stack.Screen name="Fashion" component={Pages.Fashion} />
-            </Stack.Navigator>
+            <Tab.Navigator>
+                <Tab.Screen name="Nature" component={Pages.Nature} />
+                <Tab.Screen name="Architecture" component={Pages.Architecture} />
+                <Tab.Screen name="Fashion" component={Pages.Fashion} />
+            </Tab.Navigator>
         </NavigationContainer>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
 import SplashScreen from 'react-native-splash-screen';
 import App from './app';
@@ -12,7 +12,9 @@ const PhotoSharingApp = () =>{
     }, [])
 
     return (
-        <App />
+        <SafeAreaView style={{flex: 1}}>
+            <App />
+        </SafeAreaView>
     )
 }
 
