be108f716c1836c6dd80b17544c442b82449c04b
Neil Anthony Te <neilanthonyte@Neils-MacBook-Pro.local>
2022-02-13 18:07:03 +0800

    save

--- src/lib/DownloadBtn/hooks.js
+++ src/lib/DownloadBtn/hooks.js
@@ -1,21 +1,7 @@
 import {Alert, Platform, Linking} from 'react-native';
 import RNFetchBlob from 'rn-fetch-blob';
+import RNFS from 'react-native-fs';
 import {updateState, getStates, getState} from 'my-react-global-states';
-
-const configOptions = Platform.select({
-    ios: {
-      notification: true,
-      path: RNFetchBlob.fs.dirs.DocumentDir
-    },
-    android: {
-        path: RNFetchBlob.fs.dirs.PictureDir,
-        addAndroidDownloads: {
-            useDownloadManager: true,
-            mediaScannable: true,
-            notification: true,
-        },
-    },
-});
 
 let pCount = 0;
 
@@ -54,25 +40,25 @@ const runProgress = (limit, payload = null) => {
 
 export const onDownload = async (downloadURL) => {
     try {
+        const splitUrl = downloadURL.split('/');
+        const fileName = splitUrl[splitUrl.length - 1];
+
         if(Platform.OS === 'ios') {
             updateState('show_download_bar', true, true);
             runProgress(50); // run a fake initial progress
-            const res = await downloadImg(downloadURL);
-            const splitUrl = downloadURL.split('/');
-            const fileName = splitUrl[splitUrl.length - 1];
+            const res = await downloadImg(downloadURL, fileName);
 
             runProgress(100, {...res, fileName}); // if download success continue the progress to 100%
         } else {
-            console.log(RNFetchBlob.fs.dirs.DocumentDir);
-            // const res = await downloadImg(downloadURL);
-            // console.log(res);
-            // Alert.alert(
-            //     "Download Success",
-            //     `${payload.fileName} has been successfully downloaded and saved under \n\n${payload.location}`,
-            //     [
-            //         { text: "OK", onPress: () => {} }
-            //     ]
-            // );
+            const res = await downloadImg(downloadURL, fileName);
+
+            Alert.alert(
+                "Download Success",
+                `${fileName} has been successfully downloaded and saved under \n\n${res.location}`,
+                [
+                    { text: "OK", onPress: () => {} }
+                ]
+            );
         }
     } catch (error) {
         //if error close the progress bar
@@ -81,8 +67,26 @@ export const onDownload = async (downloadURL) => {
     }
 }
 
-export const downloadImg = async (downloadURL) => {
+export const downloadImg = async (downloadURL, fileName) => {
     try {
+        const downloadDestAndroid = `${RNFS.ExternalStorageDirectoryPath}/Download/${fileName}`;
+        const downloadDestIOS = `${RNFS.LibraryDirectoryPath}/${fileName}`;
+        const configOptions = Platform.select({
+            ios: {
+                notification: true,
+                path: downloadDestIOS
+            },
+            android: {
+                path: downloadDestAndroid,
+                addAndroidDownloads: {
+                    useDownloadManager: true,
+                    mediaScannable: true,
+                    notification: true,
+                    path: downloadDestAndroid,
+                },
+            },
+        });
+
         const downloadResult = await RNFetchBlob.config(configOptions).fetch('GET', downloadURL ,{
             // headers
         });
