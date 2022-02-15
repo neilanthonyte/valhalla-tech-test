3cbdd09bc9f699417453ea68077ebbaf88833a7f
Neil Anthony Te <neilanthonyte@Neils-MacBook-Pro.local>
2022-02-15 17:27:50 +0800

    save

--- src/lib/DownloadBtn/__tests__/index.js
+++ src/lib/DownloadBtn/__tests__/index.js
@@ -1,10 +1,35 @@
 import React from "react";
 import {render} from '@testing-library/react-native';
-// import DownloadButton from '../index';
+import DownloadButton from '../index';
+
+jest.mock('rn-fetch-blob', () => {
+    return {
+        __esModule: true,
+        default: {
+            fs: {
+                unlink: jest.fn(),
+            },
+        },
+    };
+});
+
+jest.mock('react-native-fs', () => {
+    return {
+        __esModule: true,
+        ExternalStorageDirectoryPath: '',
+        LibraryDirectoryPath: ''
+    };
+});
+
+jest.mock('my-react-global-states', () => {
+    return {
+        __esModule: true,
+        updateState: () => {}
+    };
+});
 
 it('should render download button', () => {
-    // const {getAllByText} = render(<DownloadButton />);
+    const {getAllByText} = render(<DownloadButton />);
 
-    // expect(getAllByText('Download').length).toBe(1);
-    expect(true).toBe(true);
+    expect(getAllByText('Download').length).toBe(1);
 })
